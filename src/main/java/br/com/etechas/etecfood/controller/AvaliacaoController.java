bfcb35af21ac97f822c054ac24bf8ac1e78e7766
Etec Prof. Horácio Augusto da Silveira <you@example.com>
2025-11-17 13:34:42 -0300

    Adicionei injeção do AvalicacaoRepository e endpoint GET listar, Feito por Calebe

--- src/main/java/br/com/etechas/etecfood/controller/AvaliacaoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/AvaliacaoController.java
@@ -1,4 +1,21 @@
 package br.com.etechas.etecfood.controller ;
 
+import br.com.etechas.etecfood.entity.Avaliacao;
+import br.com.etechas.etecfood.repository.AvaliacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
 public class AvaliacaoController {
+    @Autowired
+    private AvaliacaoRepository avaliacaoRepository;
+
+
+    @GetMapping
+    public List<Avaliacao> listar() {
+        return avaliacaoRepository.findAll();
+    }
+
 }
