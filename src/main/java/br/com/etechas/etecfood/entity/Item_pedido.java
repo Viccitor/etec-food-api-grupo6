c76ca4fee16b41978c5c0f7797446bc019ce3ca4
Caua Santos do Nascimento <caua.nasc.sp@gmail.com>
2025-11-16 22:05:28 -0300

    Adicionando comentario com o nome dos integrantes do grupo e seus respectivos github na classe Item_pedido

--- src/main/java/br/com/etechas/etecfood/entity/Item_pedido.java
+++ src/main/java/br/com/etechas/etecfood/entity/Item_pedido.java
@@ -1,3 +1,13 @@
+/*
+ * Nomes:
+ * Victor de Melo - Github: Viccitor
+ * Miguel Angel - Github: miguel320007
+ * Anderson Filipim - Github: AndersonFilipimdeSousaFilho
+ * Gabriel de Albuquerque - Github: GabrieldeAlbuquerqueMagalhaes
+ * Jonatas Calebe - Github: calebe-007
+ * Cauã Santos - Github: CauaSantos41
+ * */
+
 package br.com.etechas.etecfood.entity;
 
 import jakarta.persistence.*;
