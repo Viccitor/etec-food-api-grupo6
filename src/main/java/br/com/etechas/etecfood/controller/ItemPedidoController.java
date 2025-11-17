718969ce0f66cd8ed50f791d4dc42efa90ac8658
Gabriel <paizinhogabriel@gmail.com>
2025-11-17 11:32:04 -0300

    Criando o List

--- src/main/java/br/com/etechas/etecfood/controller/ItemPedidoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/ItemPedidoController.java
@@ -1,4 +1,11 @@
 package br.com.etechas.etecfood.controller;
 
 public class ItemPedidoController {
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    @GetMapping
+    public List<Item_pedido> listar() {
+        return itemPedidoRepository.findAll();
+    }
 }
